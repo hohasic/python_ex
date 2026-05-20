f6f558a4e927560ff48f69afc39402c0057eefe0
hohasic <hohasic@gmail.com>
2026-05-20 10:36:05 +0900

    00002

--- 20260519ex/ex001/memberService.py
+++ 20260519ex/ex001/memberService.py
@@ -8,6 +8,14 @@ DEV_MOD = True
 
 members = {}            # Database
 
+if DEV_MOD:
+    members['gildong'] = {
+        'uId': 'gildong',
+        'uPw': '1234',
+        'uMail': 'gildong@gmail.com',
+        'uPhone': '010-1234-5678'
+    }
+
 flag = True
 while flag:
     selectedMenuNum = int(input('1.회원가입    2.로그인    3.나의 정보 출력     4.모든 회원 정보 출력    99.종료'))
@@ -19,6 +27,7 @@ while flag:
         uPhone = input('Input member PHONE: ')
 
         members[uId] = {
+            'uId': uId,
             'uPw': uPw,
             'uMail': uMail,
             'uPhone': uPhone
@@ -29,9 +38,38 @@ while flag:
         if DEV_MOD: print(f'members: {members}')
 
     elif selectedMenuNum == SIGN_IN:            # 2.로그인 
-        pass
+        uId = input('Input member ID: ')
+        uPw = input('Input member PW: ')
+
+        if uId in members:
+            uInfo = members[uId]
+            if uInfo['uPw'] == uPw:
+                print('SIGN-IN SUCCESS!!')
+            else:
+                print('SIGN-IN FAIL!!')
+        else:
+            print('존재 하지 않은 ID입니다. 다시 확인하세요.')
+        
+
     elif selectedMenuNum == PRINT_MY_INFO:      # 3.나의 정보 출력  
-        pass
+        uId = input('Input member ID: ')
+        uPw = input('Input member PW: ')
+
+        if uId in members:
+            uInfo = members[uId]
+            if uInfo['uPw'] == uPw:
+                print('SIGN-IN SUCCESS!!')
+
+                print('-' * 30)
+                for key, value in uInfo.items():
+                    print(f'{key}: {value}')
+                print('-' * 30)
+
+            else:
+                print('SIGN-IN FAIL!!')
+        else:
+            print('존재 하지 않은 ID입니다. 다시 확인하세요.')
+
     elif selectedMenuNum == PRINT_ALL_MEMBER_INFO:      # 4.모든 회원 정보 출력
         pass
     elif selectedMenuNum == SYSTEM_SHUTDOWN:    # 99.종료
